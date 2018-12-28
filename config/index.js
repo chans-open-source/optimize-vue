1373be224b3ae622d7cc38ebefec4584ef39678b
Changeden <changeden520@gmail.com>
2018-12-28 11:20:46 +0800

    加入环境配置注释

--- config/index.js
+++ config/index.js
@@ -1,8 +1,8 @@
 const pages = require('./pages');
-const port = 8080;
+const port = 8080; // 测试环境端口
 const baseUrl = process.env.NODE_ENV === 'production'
-  ? '/p/'
-  : '/d/';
+  ? '/p/' // 生产环境的子路径
+  : '/d/'; // 开发环境的子路径
 module.exports = {
   pages, port, baseUrl
 };
