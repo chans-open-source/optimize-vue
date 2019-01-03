e3e50b47ddbece7059e3885328de3131f4014673
Changeden <changeden520@gmail.com>
2019-01-03 10:21:49 +0800

    优化Initiator的屏幕高度计算

--- src/utils/Initiator.js
+++ src/utils/Initiator.js
@@ -3,12 +3,37 @@ import VuexUtils from './VuexUtils';
 import BrowserUtils from './BrowserUtils';
 import WindowContext from '../libs/WindowContext';
 
+// UI图宽度标准(iPhone 6 宽度)
 const BASE_WIDTH = 750; // 24px = 1rem
+
+/**
+ * 设置样式
+ * @param e DOM节点
+ * @param k 样式键
+ * @param v 样式值
+ * */
 const setStyle = (e, k, v) => {
   e.forEach(i => {
     window.document.getElementsByTagName(i)[0]['style'][k] = v;
   });
 };
+
+/**
+ * 从可变参数中获取大于0的最小值
+ * eg: min(1, 2, 3, 0); return 1;
+ * */
+const min = function () {
+  const length = arguments.length;
+  let min = arguments[0];
+  for (let i = 0; i < length; i++) {
+    const arg = arguments[i];
+    if (arg > 0) {
+      min = Math.min(min, arg);
+    }
+  }
+  return min;
+};
+
 export default class Initiator extends WindowContext {
 
   static registerApp (app) {
@@ -20,12 +45,15 @@ export default class Initiator extends WindowContext {
   }
 
   static setWindowSize () {
-    const { width, height } = this.$w.screen;
-    setStyle(['html'], 'fontSize', `${width / BASE_WIDTH * 24}px`);
-    setStyle(['html', 'body'], 'minHeight', `${height}px`);
+    const { screen, innerHeight, innerWidth, outerHeight, outerWidth } = this.$w;
+    const { width, height, availHeight, availWidth } = screen;
+    const _width = min(innerWidth, outerWidth, width, availWidth);
+    const _height = min(innerHeight, outerHeight, height, availHeight);
+    setStyle(['html'], 'fontSize', `${_width / BASE_WIDTH * 24}px`);
+    setStyle(['html', 'body'], 'minHeight', `${_height}px`);
     VuexUtils.emmit(this.$app, 'setWindowSize', {
-      width,
-      height
+      width: _width,
+      height: _height
     }).then().catch();
     window.remScale = (window.fontSize = parseFloat(window.getComputedStyle(window.document.querySelector('html')).fontSize)) / 24;
   }
